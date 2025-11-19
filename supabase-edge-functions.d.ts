b8dd1a75e7a4af1357f125a8d9c243af519cd38c
rodineynb <alerod.mkt@gmail.com>
2025-11-19 10:32:08 -0300

    teste
    teste

--- supabase-edge-functions.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "https://deno.land/std@0.190.0/http/server.ts" {
-  export function serve(handler: (request: Request) => Promise<Response> | Response): Promise<void>;
-}
-
-declare module "https://esm.sh/@supabase/supabase-js@2.45.0" {
-  import { SupabaseClient, createClient as originalCreateClient } from '@supabase/supabase-js';
-  export const createClient: typeof originalCreateClient;
-  export { SupabaseClient };
-}
